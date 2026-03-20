f4a53b5ace870ffd73ebcda9da62014e4c9ae5d6
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-03-20 12:12:16 +0100

    Add Trip model with admin integration

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField(max_length=100)
+    max_people = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
