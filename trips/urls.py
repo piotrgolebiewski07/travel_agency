ae6ba55d36ab79ee252492b5b0f04fb25fc15491
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-03-24 12:34:00 +0100

    Add trip detail view, improve templates and rotuting

--- trips/urls.py
+++ trips/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('<int:pk>/', views.trip_detail, name="detail"),
 ]
 
--- trips/views.py
+++ trips/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Trip
+
+
+def trip_detail(request, pk):
+    trip = get_object_or_404(Trip, pk=pk)
+    return render(request, "trips/detail.html", {"trip": trip})
 
 
 def home(request):
