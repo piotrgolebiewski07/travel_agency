9a4cd1fa336b2fb806554f1d2a97af4e1b9bd8e3
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-04-08 10:09:56 +0200

    Add pagination to DRF settings

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "MEDIA"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+}
