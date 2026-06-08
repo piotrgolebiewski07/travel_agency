7057956a76961c2dff5ebae065545fc83452c7dc
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-06-08 11:03:47 +0200

    Add database fallback for local development

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
 
